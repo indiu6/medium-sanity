34e879553b472b79b0bf7361d02b982d1d2e4e1f
indiu6 <hewas6@gmail.com>
2022-01-27 00:16:05 -0500

    add post detail page but error

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function Home({ posts }: Props) {
-  console.log(posts)
+  // console.log(posts)
 
   return (
     <div className="mx-auto max-w-7xl">
@@ -42,18 +42,22 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
-      <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
               {post.mainImage && (
-                <img src={urlFor(post.mainImage).url()!} alt="" />
+                <img
+                  className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
               )}
 
               <div className="flex justify-between bg-white p-5">
                 <div>
-                  <p>{post.title}</p>
-                  <p>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
                     {post.description} by {post.author.name}
                   </p>
                 </div>
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,89 @@
+import { GetStaticProps } from 'next'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
+
+interface Props {
+  post: Post
+}
+
+function Post({ post }: Props) {
+  // console.log(post)
+
+  return (
+    <main>
+      <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+      </article>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    slug {
+      current,
+    },
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt
+    title,
+    author -> {
+      name,
+      image
+    },
+    'comments': *[
+      _type == "comment" &&
+      post._ref = ^._id &&
+      approved == true],
+    description,
+    mainImage,
+    slug,
+    body
+  }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // after 60 sec, it'll update the old cached version
+  }
+}
